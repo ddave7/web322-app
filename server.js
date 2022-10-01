57558889880260cf72ec08b15b518b95dd175ea7
Devarsh Dave <ddave7@myseneca.ca>
2022-09-30 20:38:46 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,73 @@
+/***********************************************************************
+**********
+* WEB322 – Assignment 02
+* I declare that this assignment is my own work in accordance with Seneca Academic
+Policy. No part * of this assignment has been copied manually or electronically from any
+other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Devarsh Yatinbhai Dave
+  Student ID: 163077217
+  Date:30/09/2022
+
+*
+* Online (cyclic) Link: https://cloudy-newt-tutu.cyclic.app/about
+
+************************************************************************
+********/
+
+
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+var path = require('path');
+var blogservice = require(__dirname + '/blog-service');
+app.use(express.static('public'));
+
+onHttpStart = () => {
+    console.log('Express http server listening on ' + HTTP_PORT);
+}
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.redirect('/about')
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname + "/views/about.html"));
+});
+
+app.get("/blog", (req, res) => {
+    blogservice.getPublishedPosts().then((data) => {
+        res.json({ data });
+    }).catch((err) => {
+        res.json({ message: err });
+    })
+});
+
+app.get("/data/posts.json", (req, res) => {
+    blogservice.getAllPosts().then((data) => {
+        res.json({ data });
+    }).catch((err) => {
+        res.json({ message: err });
+    })
+});
+
+app.get("/data/categories.json", (req, res) => {
+    blogservice.getCategories().then((data) => {
+        res.json({ data });
+    }).catch((err) => {
+        res.json({ message: err });
+    })
+});
+
+app.get('*', function(req, res) {
+    res.status(404).send("Page Not Found!");
+});
+
+blogservice.initialize().then(() => {
+    app.listen(HTTP_PORT, onHttpStart());
+}).catch(() => {
+    console.log("ERROR : From starting the server");
+});
