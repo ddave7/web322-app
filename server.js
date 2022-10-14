28bd1bbe26d4510b5a4ab6d62e64b94220e37e6e
Devarsh Dave <ddave7@myseneca.ca>
2022-10-14 16:36:14 -0400

    assignment3 last commit

--- server.js
+++ server.js
@@ -13,15 +13,27 @@ Policy. No part
 ********************************************************************************/
 
 var express = require("express");
-var HTTP_PORT = process.env.PORT || 8080;
 var app = express();
 var path = require('path');
-var blogservice = require(__dirname + '/blog-service');
-app.use(express.static('public'));
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
+var blogservice = require(__dirname + '/blog-service.js');
 
-onHttpStart = () => {
+var HTTP_PORT = process.env.PORT || 8080;
+
+function onHttpStart() {
     console.log('Express http server listening on ' + HTTP_PORT);
 }
+
+cloudinary.config({
+    cloud_name: 'djimq7jgi',
+    api_key: '356968492474775',
+    api_secret: 'QsnKPtzL0UyxUabsJ4xrY1-iULs',
+    secure: true
+});
+
+const upload = multer() // no { storage: storage }
 
 app.use(express.static('public'));
 
@@ -31,18 +43,6 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname + "/views/about.html"));
-});
-
-app.get('/posts/add', (req, res) => {
-    res.sendFile(path.join(__dirname + "/views/addPost.html"));
-});
-
-app.get("/blog", (req, res) => {
-    blogservice.getPublishedPosts().then((data) => {
-        res.json({ data });
-    }).catch((err) => {
-        res.json({ message: err });
-    })
 });
 
 app.get("/data/posts.json", (req, res) => {
@@ -60,6 +60,95 @@ app.get("/data/categories.json", (req, res) => {
         res.json({ message: err });
     })
 });
+
+app.get("/blog", function(req, res) {
+    blogservice.getPublishedPosts().then(function(data) {
+        res.json({ data });
+    }).catch(function(err) {
+        res.json({ message: err });
+    })
+});
+
+app.get("/posts", function(req, res) {
+
+    if (req.query.category) {
+        blog.getPostsByCategory(req.query.category).then((data) => {
+            res.json(data);
+        }).catch(function(err) {
+            res.json({ message: err });
+        })
+    } else if (req.query.minDate) {
+        blog.getPostsByMinDate(req.query.minDate).then((data) => {
+            res.json(data);
+        }).catch(function(err) {
+            res.json({ message: err });
+        })
+    } else {
+        blogservice
+            .getAllPosts()
+            .then(function(data) {
+                res.json(data);
+            })
+            .catch(function(err) {
+                res.json({ message: err });
+            });
+    }
+});
+
+app.get('/post/:id', (req, res) => {
+    blog.getPostById(req.params.id).then((data) => {
+
+        res.json(data);
+    }).catch(function(err) {
+        res.json({ message: err });
+    });
+
+
+});
+
+app.get("/categories", function(req, res) {
+    blogservice.getCategories().then(function(data) {
+        res.json({ data });
+    }).catch(function(err) {
+        res.json({ message: err });
+    })
+});
+
+
+app.get('/posts/add', function(req, res) {
+    res.sendFile(path.join(__dirname + "/views/addPost.html"));
+});
+
+app.post('/posts/add', upload.single("featureImage"), (req, res) => {
+
+    let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+            let stream = cloudinary.uploader.upload_stream(
+                (error, result) => {
+                    if (result) {
+                        resolve(result);
+                    } else {
+                        reject(error);
+                    }
+                }
+            );
+            streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+    };
+    async function upload(req) {
+        let result = await streamUpload(req);
+        console.log(result);
+        return result;
+    }
+    upload(req).then((uploaded) => {
+        req.body.featureImage = uploaded.url;
+        blogservice.addPost(req.body).then(() => {
+            res.redirect('/posts');
+        }).catch((data) => { res.send(data); })
+    });
+
+})
+
 
 app.get('*', function(req, res) {
     res.status(404).send("Page Not Found!");
