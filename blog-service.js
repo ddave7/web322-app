cfae85438115e148fbcef4cc160ef2513b8f5538
Devarsh Dave <ddave7@myseneca.ca>
2022-10-14 13:24:43 -0400

    assignment3 second commit

--- blog-service.js
+++ blog-service.js
@@ -40,9 +40,11 @@ exports.getPublishedPosts = () => {
 
         if (publishposts.length == 0) {
             reject('no results returned');
+        } else {
+            resolve(publishposts)
         }
-        resolve(publishposts);
-    })
+
+    });
 };
 
 exports.getCategories = () => {
@@ -52,5 +54,90 @@ exports.getCategories = () => {
         } else {
             resolve(categories);
         }
-    })
+    });
 };
+
+
+module.exports.addPost = (postData) => {
+    return new Promise(function(resolve, reject) {
+        try {
+            postData.published = (postData.published) ? true : false;
+            postData.id = posts.length + 1;
+            posts.push(postData);
+            resolve(postData);
+        } catch (err) {
+            reject();
+        }
+    });
+}
+
+getPostsByCategory = (category) => {
+
+    {
+        return new Promise(function(resolve, reject) {
+            var category_posts = [];
+
+            var j = 0;
+
+
+            for (var i = 0; i < posts.length; i++) {
+                if (posts[i].category == category) {
+                    category_posts[j++] = posts[i];
+                }
+
+            }
+            if (category_posts.length == 0) {
+                reject("no results returned");
+            } else {
+                resolve(category_posts);
+            }
+
+
+        });
+    }
+}
+
+getPostsByMinDate = (minDateStr) => {
+
+    {
+        return new Promise(function(resolve, reject) {
+            var date_posts = [];
+
+            var j = 0;
+
+
+            for (var i = 0; i < posts.length; i++) {
+                if (new Date(posts[i].postDate) >= new Date(minDateStr)) {
+                    date_posts[j++] = posts[i];
+                }
+
+            }
+            if (date_posts.length == 0) {
+                reject("no results returned");
+            } else {
+                resolve(date_posts);
+            }
+
+
+        });
+    }
+}
+
+getPostById = (id) => {
+    return new Promise((resolve, reject) => {
+        var id_posts = [];
+        var j = 0;
+        for (var i = 0; i < posts.length; i++) {
+            if (posts[i].id == id) {
+                id_posts[j++] = posts[i];
+            }
+        }
+
+        if (id_posts.length == 0) {
+            reject("no results returned");
+        } else {
+            resolve(id_posts);
+        }
+
+    });
+}
--- server.js
+++ server.js
@@ -1,32 +1,39 @@
-/***********************************************************************
-**********
-* WEB322 – Assignment 02
+/*********************************************************************************
+* WEB322 – Assignment 03
 * I declare that this assignment is my own work in accordance with Seneca Academic
-Policy. No part * of this assignment has been copied manually or electronically from any
-other source
+Policy. No part
+* Of this assignment has been copied manually or electronically from any other source
 * (including 3rd party web sites) or distributed to other students.
 *
-* Name: Devarsh Yatinbhai Dave
-  Student ID: 163077217
-  Date:30/09/2022
+* Name: Devarsh Yatinbhai Dave ID: 163077217 Date: 10/14/2022
 
 *
-* Online (cyclic) Link: https://busy-red-perch-tam.cyclic.app/about
+* Online (Cyclic) Link: https://busy-red-perch-tam.cyclic.app/about
+*
+********************************************************************************/
 
-************************************************************************
-********/
-
-
-var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 var path = require('path');
-var blogservice = require(__dirname + '/blog-service');
-app.use(express.static('public'));
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
+var blogservice = require(__dirname + '/blog-service.js');
 
-onHttpStart = () => {
+var HTTP_PORT = process.env.PORT || 8080;
+
+function onHttpStart() {
     console.log('Express http server listening on ' + HTTP_PORT);
 }
+
+cloudinary.config({
+    cloud_name: 'djimq7jgi',
+    api_key: '356968492474775',
+    api_secret: 'QsnKPtzL0UyxUabsJ4xrY1-iULs',
+    secure: true
+});
+
+const upload = multer() // no { storage: storage }
 
 app.use(express.static('public'));
 
@@ -38,29 +45,94 @@ app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname + "/views/about.html"));
 });
 
-app.get("/blog", (req, res) => {
-    blogservice.getPublishedPosts().then((data) => {
+app.get("/blog", function(req, res) {
+    blogservice.getPublishedPosts().then(function(data) {
         res.json({ data });
-    }).catch((err) => {
+    }).catch(function(err) {
         res.json({ message: err });
     })
 });
 
-app.get("/data/posts.json", (req, res) => {
-    blogservice.getAllPosts().then((data) => {
+app.get("/posts", function(req, res) {
+
+    if (req.query.category) {
+        blog.getPostsByCategory(req.query.category).then((data) => {
+            res.json(data);
+        }).catch(function(err) {
+            res.json({ message: err });
+        })
+    } else if (req.query.minDate) {
+        blog.getPostsByMinDate(req.query.minDate).then((data) => {
+            res.json(data);
+        }).catch(function(err) {
+            res.json({ message: err });
+        })
+    } else {
+        blogservice
+            .getAllPosts()
+            .then(function(data) {
+                res.json(data);
+            })
+            .catch(function(err) {
+                res.json({ message: err });
+            });
+    }
+});
+
+app.get('/post/:id', (req, res) => {
+    blog.getPostById(req.params.id).then((data) => {
+
+        res.json(data);
+    }).catch(function(err) {
+        res.json({ message: err });
+    });
+
+
+});
+
+app.get("/categories", function(req, res) {
+    blogservice.getCategories().then(function(data) {
         res.json({ data });
-    }).catch((err) => {
+    }).catch(function(err) {
         res.json({ message: err });
     })
 });
 
-app.get("/data/categories.json", (req, res) => {
-    blogservice.getCategories().then((data) => {
-        res.json({ data });
-    }).catch((err) => {
-        res.json({ message: err });
-    })
+
+app.get('/posts/add', function(req, res) {
+    res.sendFile(path.join(__dirname + "/views/addPost.html"));
 });
+
+app.post('/posts/add', upload.single("featureImage"), (req, res) => {
+
+    let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+            let stream = cloudinary.uploader.upload_stream(
+                (error, result) => {
+                    if (result) {
+                        resolve(result);
+                    } else {
+                        reject(error);
+                    }
+                }
+            );
+            streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+    };
+    async function upload(req) {
+        let result = await streamUpload(req);
+        console.log(result);
+        return result;
+    }
+    upload(req).then((uploaded) => {
+        req.body.featureImage = uploaded.url;
+        blogservice.addPost(req.body).then(() => {
+            res.redirect('/posts');
+        }).catch((data) => { res.send(data); })
+    });
+
+})
+
 
 app.get('*', function(req, res) {
     res.status(404).send("Page Not Found!");
